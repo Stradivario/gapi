91d29c374967e7d8238ff5b34d0a14885ab587ac
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-04-01 14:47:51 +0300

    preparation

--- build-versions.js
+++ build-versions.js
@@ -1,37 +1,41 @@
-const { writeFileSync } = require('fs');
-const { join } = require('path');
-const { lstatSync, readdirSync, readFileSync } = require('fs');
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+const { lstatSync, readdirSync, readFileSync } = require("fs");
 
-const package = require('./package.json');
+const package = require("./package.json");
 let buildVersion = package.version;
 
 const isDirectory = source => lstatSync(source).isDirectory();
-const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
+const getDirectories = source =>
+  readdirSync(source)
+    .map(name => join(source, name))
+    .filter(isDirectory);
 const repos = [];
 
-getDirectories(__dirname)
-.forEach(dir => {
-  if (dir.includes('.git')) {
+getDirectories(__dirname).forEach(dir => {
+  if (dir.includes(".git")) {
     return;
   }
   repos.push({
-    name: JSON.parse(readFileSync(dir + '/package.json', 'utf8')).name,
+    name: JSON.parse(readFileSync(dir + "/package.json", "utf8")).name,
     directory: dir
   });
-})
+});
 
 function changeVersion(packages, operation) {
   if (packages) {
     const matches = [];
 
-    for(let dep in packages) {
+    for (let dep in packages) {
       repos.forEach(repo => {
-        if(dep === repo.name) {
-    
-          packages[repo.name] = '^' + buildVersion;
-          matches.push({name: repo.name, packageVersion: packages[repo.name]})
+        if (dep === repo.name) {
+          packages[repo.name] = "^" + buildVersion;
+          matches.push({
+            name: repo.name,
+            packageVersion: packages[repo.name]
+          });
         }
-      })
+      });
     }
     if (matches.length) {
       console.log(operation);
@@ -41,28 +45,37 @@ function changeVersion(packages, operation) {
   }
 }
 
-repos
-.forEach(r => {
-  const rawFile = readFileSync(r.directory + '/package.json', 'utf8');
+repos.forEach(r => {
+  const rawFile = readFileSync(r.directory + "/package.json", "utf8");
   const package = JSON.parse(rawFile);
-
-  console.log('### ', r.name, '###');
-  const dependencies = changeVersion(package.dependencies, 'dependencies: ') || [];
-  const devDependencies = changeVersion(package.devDependencies, 'devDependencies: ') || [];
-  const peerDependencies = changeVersion(package.peerDependencies, 'peerDependencies') || [];
-
-  const matches = [
-    ...dependencies,
-    ...devDependencies,
-    ...peerDependencies
-  ];
-  if (matches.length) {
-    console.log('Found dependencies!');
-    writeFileSync(r.directory + '/package.json', JSON.stringify(package, null, 2), 'utf8');
-    console.log('---------------------------------------------');
+  console.log("### ", r.name, "###");
+  if (process.argv[2] === "--package") {
+    package.version = buildVersion;
+    writeFileSync(
+      r.directory + "/package.json",
+      JSON.stringify(package, null, 2),
+      "utf8"
+    );
   } else {
-    console.log('No Matches found')
+    const dependencies =
+      changeVersion(package.dependencies, "dependencies: ") || [];
+    const devDependencies =
+      changeVersion(package.devDependencies, "devDependencies: ") || [];
+    const peerDependencies =
+      changeVersion(package.peerDependencies, "peerDependencies") || [];
+
+    const matches = [...dependencies, ...devDependencies, ...peerDependencies];
+
+    if (matches.length) {
+      console.log("Found dependencies!");
+      writeFileSync(
+        r.directory + "/package.json",
+        JSON.stringify(package, null, 2),
+        "utf8"
+      );
+      console.log("---------------------------------------------");
+    } else {
+      console.log("No Matches found");
+    }
   }
-
 });
-
