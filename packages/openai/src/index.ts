ec660f9542909f16b5b8bda19ae39dee00c48352
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2023-02-25 12:03:03 +0200

    feat(openai): added open ai module with simple graphql mutation

---
+++ packages/openai/src/index.ts
@@ -0,0 +1,5 @@
+export * from './openai.controller';
+export * from './openai.module';
+export * from './openai.tokens';
+export * from './types/create-completion-input.type';
+export * from './types/create-completion.type';
