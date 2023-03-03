e5f96352b15617cd5faeb2f31e571ff3bde46806
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2023-03-03 12:32:23 +0200

    feat(openai): created chat completion node

--- packages/openai/src/types/create-completion.type.ts
+++ packages/openai/src/types/create-completion.type.ts
@@ -24,7 +24,19 @@ export const CreateCompletionChoicesType = new GraphQLObjectType({
   name: 'CreateCompletionChoicesType',
   fields: () => ({
     text: {
+      description: 'This property can be defined using createCompletion method',
       type: GraphQLString,
+    },
+    message: {
+      description:
+        'This property can be defined using createChatCompletion method',
+      type: new GraphQLObjectType({
+        name: 'CreateChatCompletionMessageType',
+        fields: () => ({
+          role: { type: GraphQLString },
+          content: { type: GraphQLString },
+        }),
+      }),
     },
     index: {
       type: GraphQLString,
